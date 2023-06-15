f3b038903ff60bf2627b1c91def4beaef902ef9e
TheMeinerLP <p.glanz@madfix.me>
2023-06-15 20:26:10 +0200

    Add name and display item

--- src/main/java/dev/themeinerlp/bettergopaint/fawe/brushes/BucketBrush.java
+++ src/main/java/dev/themeinerlp/bettergopaint/fawe/brushes/BucketBrush.java
@@ -1,5 +1,6 @@
 package dev.themeinerlp.bettergopaint.fawe.brushes;
 
+import com.destroystokyo.paper.profile.ProfileProperty;
 import com.sk89q.worldedit.EditSession;
 import com.sk89q.worldedit.MaxChangedBlocksException;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
@@ -9,8 +10,15 @@ import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.math.Vector3;
 import com.sk89q.worldedit.world.block.BaseBlock;
 import dev.themeinerlp.bettergopaint.fawe.util.BrushSettings;
+import dev.themeinerlp.bettergopaint.utils.Constants;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.Plugin;
+
+import java.util.UUID;
 
 public class BucketBrush implements BetterBrush {
 
@@ -18,10 +26,20 @@ public class BucketBrush implements BetterBrush {
     private final BrushSettings brushSettings;
     private final Plugin plugin;
 
+    private final ItemStack itemStack;
+
     public BucketBrush(Player player, BrushSettings brushSettings, Plugin plugin) {
         this.player = player;
         this.brushSettings = brushSettings;
         this.plugin = plugin;
+
+        itemStack = new ItemStack(Material.PLAYER_HEAD);
+        if (itemStack.getItemMeta() instanceof SkullMeta skullMeta) {
+            var profile = Bukkit.createProfile(UUID.randomUUID());
+            profile.setProperty(new ProfileProperty("textures", Constants.HEAD_BUCKET));
+            skullMeta.setPlayerProfile(profile);
+            itemStack.setItemMeta(skullMeta);
+        }
     }
 
     @Override
@@ -67,6 +85,16 @@ public class BucketBrush implements BetterBrush {
     @Override
     public BrushSettings settings() {
         return this.brushSettings;
+    }
+
+    @Override
+    public String getName() {
+        return "BucketBrush";
+    }
+
+    @Override
+    public ItemStack getDisplayItem() {
+        return itemStack;
     }
 
 }
